a7b539c4018d9494e1a2f4bbe8b2a87d6dda4c1c
filifarne <117976401+filifarne@users.noreply.github.com>
2022-11-11 11:03:10 +0100

    Upload Esercizio3, Filippo Farnesi, 11/11/2022

---
+++ Esercizio3.java
@@ -0,0 +1,29 @@
+
+
+class Esrcizio.3{
+    fun main(){
+        
+        // Dichiarazione delle variabili
+        char letter;
+        int N1;
+        double N2;
+        int N3;
+        boolean Bool1;
+        boolean Bool2;
+
+        // Valorizzazione delle variabili
+        letter='b';
+        N1=letter;
+        N2=123.34;
+        N3=1;
+        Bool1=true;
+        Bool2=false;
+
+        // Operazioni
+        N2=(N1+N2+N3)/3
+
+        // Fase di stampa
+        if Bool1==!(Bool2){System.out.print(N2)}
+
+    }   // Chiusura Main
+}
